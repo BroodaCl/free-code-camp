6b4ea58a19d9b2158d39d66c5444bd51d175a141
BroodaCL <brooda.brooda@gmail.com>
2025-09-01 20:42:35 -0400

    update use helmet to deny x-fame info

--- myApp.js
+++ myApp.js
@@ -2,13 +2,9 @@ const express = require('express');
 const helmet =require('helmet')
 const app = express();
 
-app.use(helmet.hidePoweredBy())
+app.use(helmet.hidePoweredBy());
 
-app.use(
-  helmet({
-    xFrameOptions: { action: "deny" },
-  }),
-);
+app.use(helmet.frameguard({action: 'deny'}));
 
 
 
